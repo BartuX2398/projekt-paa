e53874060cd26b16052a80ea9b882a4185ed2ceb
Ubuntu <bartek@VM1.2mnziond0bjenmh4f0rzrn5bxa.ax.internal.cloudapp.net>
2020-04-09 14:00:27 +0000

    Dodano możliwość zmiany statusu zadania

---
+++ public/javascripts/index.js
@@ -0,0 +1,24 @@
+  fetch('/tasks/updateStatus', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    },
+    body: `id=${id}&status=${status}`
+  })
+  .finally(() => {
+    window.location = '/'
+  })
+}
+
+const updateTaskStatus = (id, status) => {
+  fetch('/tasks/updateStatus', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    },
+    body: `id=${id}&status=${status}`
+  })
+  .finally(() => {
+    window.location = '/'
+  })
+}
--- store.js
+++ store.js
@@ -12,11 +12,9 @@ const init = async () => (
 module.exports = {
   init,
   createTask,
-  listTasks
+  listTasks,
+  updateTaskStatus
 }
-
-
-
 
 const createTask = async (title) => (
   new Promise((resolve, reject) => {
@@ -24,7 +22,8 @@ const createTask = async (title) => (
     const task = {
       PartitionKey: generator.String('task'),
       RowKey: generator.String(uuid.v4()),
-      title
+      title,
+      status: 'open'
     }
 
     service.insertEntity(table, task, (error, result, response) => {
@@ -32,20 +31,39 @@ const createTask = async (title) => (
     })
   })
 )
-module.exports = {
-  init,
-  createTask
-}
+
+
+
+
+
 const listTasks = async () => (
   new Promise((resolve, reject) => {
     const query = new storage.TableQuery()
-      .select(['title'])
+      .select(['RowKey', 'title', 'status'])
       .where('PartitionKey eq ?', 'task')
 
     service.queryEntities(table, query, null, (error, result, response) => {
       !error ? resolve(result.entries.map((entry) => ({
-        title: entry.title._
+        id: entry.RowKey._,
+        title: entry.title._,
+        status: entry.status._
       }))) : reject()
+    })
+  })
+)
+
+
+const updateTaskStatus = async (id, status) => (
+  new Promise((resolve, reject) => {
+    const generator = storage.TableUtilities.entityGenerator
+    const task = {
+      PartitionKey: generator.String('task'),
+      RowKey: generator.String(id),
+      status
+    }
+
+    service.mergeEntity(table, task, (error, result, response) => {
+      !error ? resolve() : reject()
     })
   })
 )
